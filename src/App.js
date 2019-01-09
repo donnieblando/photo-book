6e1424775802975d8029986efe120ce0f10ba9bb
giuseppe <Giuseppe.Livide@vistaprint.com>
2019-01-09 15:49:26 +0100

    Preparing for 2.0 MODULAR

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AboutPage from './containers/AboutPage';
 import ContactsPage from './containers/ContactsPage';
 import Generator from './containers/Generator';
 import ProjectDetailPage from './containers/ProjectDetailPage';
+import { PROJECT_IDS } from './utils/constants';
 
 class App extends Component {
   render() {
@@ -20,8 +21,23 @@ class App extends Component {
           <Route path="*/about" component={AboutPage} />
           <Route path="*/contacts" component={ContactsPage} />
           <Route exact path="*/projects" component={ProjectsPage} />
+          
+          <Route
+            path="*/projects/:project" component={ProjectDetailPage}
+          />
 
-          <Route path="*/projects/:project" component={ProjectDetailPage} />
+          <Route
+            path="*/projects/portraits"
+            render={() => <ProjectDetailPage project={PROJECT_IDS.PORTRAITS} />}
+          />
+          <Route
+            path="*/projects/benches"
+            render={() => <ProjectDetailPage project={PROJECT_IDS.BENCHES} />}
+          />
+          <Route
+            path="*/projects/livemusic"
+            render={() => <ProjectDetailPage project={PROJECT_IDS.LIVEMUSIC} />}
+          />
 
           <Route path="*/generator" component={Generator} />
         </Switch>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const IMG_EXTENSIONS = {
   JPG: '.jpg'
 };
 
+export const PROJECT_IDS = {
+  PORTRAITS: "PORTRAITS",
+  LIVEMUSIC: "LIVEMUSIC",
+  BENCHES: "BENCHES"
+}
